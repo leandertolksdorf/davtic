bb6a994b75549e68f76891414c946bf8a689caec
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2023-01-05 16:29:28 +0100

    #3 create config package with server config

---
+++ packages/config/src/index.ts
@@ -0,0 +1,9 @@
+import {DavticConfig} from "./lib/davticConfig.type";
+
+const davticConfig: DavticConfig = {
+  caldav: {
+    server: "mycaldavserver.com",
+    password: "helloworld"
+  }
+}
+export default davticConfig;
